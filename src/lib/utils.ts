f51e78bffcc4c9a540e8786c2ff883b2e262b30d
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-29 16:55:39 +0300

    Add app structure and library files

---
+++ src/lib/utils.ts
@@ -0,0 +1,17 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(date)
+}
+export function generateId(): string {
+  return Math.random().toString(36).substr(2, 9)
+}
